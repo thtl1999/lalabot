78d9d66c648a2f92b4fa679f5913fcb951117647
thtl1999 <thtl2004@hanmail.net>
2019-01-29 17:21:00 +0900

    sdvx song recommandation function

---
+++ sdvxbot/main.py
@@ -0,0 +1,32 @@
+#python 3.5.3
+import os
+import requests
+import sys
+import fileinput
+import re
+import json
+
+musiclevels = ['16','17','18','19','20']
+
+for i in musiclevels:
+    req = requests.get('http://sdvx.in/sort/sort_' + i + '.js')
+    f = open(i + '.txt', 'w', encoding='utf8')
+    f.write(req.text)
+    f.close()
+
+jsonlist = {}
+
+for i in musiclevels:
+    f = open(i + '.txt', 'r', encoding='utf8')
+    jsonlist[i] = []
+    lines = f.readlines()
+    for line in lines:
+        if '//' in line:
+            songname = line.split('//')
+            jsonlist[i].append(songname[1].replace('\n',''))
+    f.close()
+
+print(jsonlist)
+
+with open('songlist.txt', 'w') as outfile:
+    json.dump(jsonlist, outfile)
